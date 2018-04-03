0b02e112d89158c3401179e684bbfc2efe89c011
Michael Kemp <mkem114@aucklanduni.ac.nz>
2018-04-03 17:40:09 +1200

    Add command line arguments interpreter (#10)
    Add command line argument interpreter

--- main.cpp
+++ main.cpp
@@ -2,10 +2,57 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <cstring>
 
 using namespace std;
 
-int main() {
+void printHelp();
+void printVersion(string version);
+bool checkOption(string option, string input);
+
+int main(int argc, char *argv[]) {
+    const string version ("beta");
+    string outputFilename ("results.txt");
+    string inputFilename ("inputData.csv");
+    bool parrallel = true;
+    bool quiet = false;
+
+    bool inputChangeFlag = false;
+    bool outputChangeFlag = false;
+    for (int i = 0; i < argc; i++) {
+        if (inputChangeFlag) {
+            inputChangeFlag = false;
+            if (checkOption("i", argv[i-1])) {
+                inputFilename = argv[i];
+            }
+        } else if (outputChangeFlag) {
+            outputChangeFlag = false;
+            if (checkOption("o", argv[i-1])) {
+                outputFilename = argv[i];
+            }
+        } else {
+            if (checkOption("h", argv[i])) {
+                printHelp();
+                return 0;
+            } else if (checkOption("v", argv[i])) {
+                printVersion(version);
+                return 0;
+            } else if (checkOption("q", argv[i])) {
+                quiet = true;
+            } else if (checkOption("h", argv[i])) {
+                parrallel = false;
+            } else if (checkOption("i", argv[i])) {
+                inputChangeFlag = true;
+            } else if (checkOption("o", argv[i])) {
+                outputChangeFlag = true;
+            }
+        }
+    }
+
+    if (argc == 0) {
+        printHelp();
+    }
+
     ifstream input( "../data.csv" );
 
     for (string line; getline(input, line); ) {
@@ -15,14 +62,36 @@ int main() {
         while (ss >> i) {
             vect.push_back(i);
 
-            if (ss.peek() == ',')
+            if (ss.peek() == ',') {
                 ss.ignore();
+            }
         }
 
-        for (i=0; i< vect.size(); i++) {
+        for (i = 0; i < vect.size(); i++) {
             std::cout << vect.at(i) << std::endl;
         }
     }
 
     return 0;
+}
+
+void printHelp() {
+    cout << "=== ParaStats Help ==" << endl;
+    cout << "-o outputFileName" << endl;
+    cout << "-i inputFileName" << endl;
+    cout << "-h help" << endl;
+    cout << "-s sequential mode" << endl;
+    cout << "-v version" << endl;
+    cout << "-q quiet" << endl;
+}
+
+void printVersion(string version) {
+    cout << version << endl;
+}
+
+bool checkOption(string option, string input) {
+    if (input.compare("/" + option) == 0 || input.compare("-" + option) == 0) {
+        return true;
+    }
+    return false;
 }
